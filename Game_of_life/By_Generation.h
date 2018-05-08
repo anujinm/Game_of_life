c0d4ce344e9ef53e9b937ec58077b1260e5f761b
amonrs <amonsthapit@live.com>
2018-05-07 18:19:41 -0700

    made by_generation and by_queue

---
+++ Game_of_life/By_Generation.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "newGame.h"
+
+
+
+#ifndef BY_GENERATION_H
+#define BY_GENERATION_H
+
+
+
+class By_Generation : public newGame
+{
+private:
+	int zombie_mode_wait_rate;
+
+public:
+	By_Generation() {
+		//constructor
+
+	}
+	~By_Generation();
+
+
+	void set_state_generation() {
+
+	}
+
+	void rule_1() {
+
+	}
+	void rule_3() {
+
+	}
+	void rule_4() {
+
+	}
+
+
+};
+
+
+
+#endif // !BY_GENERATION_H
---
+++ Game_of_life/By_Queue.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "newGame.h"
+
+
+
+#ifndef BY_QUEUE_H
+#define BY_QUEUE_H
+
+
+
+class By_Queue
+{
+private:
+	int resurrection_rate;
+
+public:
+	By_Queue() {
+		//constructor
+
+	}
+	~By_Queue();
+
+};
+
+
+
+#endif // !BY_QUEUE_H
